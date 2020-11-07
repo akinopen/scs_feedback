551b8254c0296814cea3399ef6cd228473297ea4
Onur Güzel <guzelmu@gmail.com>
2020-11-07 13:27:54 +0300

    Customize admin pages

--- feedback/admin.py
+++ feedback/admin.py
@@ -7,9 +7,16 @@ __all__ = ("FeedbackAdmin", "RequestAdmin")
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
-    pass
+    exclude = ("start_doing", "continue_doing", "stop_doing")
+    list_display = ("author", "recipient", "created_at")
+    list_filter = ("created_at",)
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("sender", "created_at")
+    list_filter = ("created_at",)
+    readonly_fields = ("sender", "recipients")
